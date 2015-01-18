704df39755626bcf2a66863592e4fd5886d0e246
Matt Kula <matthewrkula@gmail.com>
2015-01-17 20:54:44 -0500

    Removed alllll the threads

--- app/src/main/java/dwai/datawave/MainActivity.java
+++ app/src/main/java/dwai/datawave/MainActivity.java
@@ -4,6 +4,7 @@ import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -23,10 +24,13 @@ import be.tarsos.dsp.pitch.PitchProcessor;
 
 public class MainActivity extends ActionBarActivity {
 
+    Handler handler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        handler = new Handler();
         final EditText urlEditText = ((EditText) findViewById(R.id.urlEditText));
         urlEditText.setOnKeyListener(new View.OnKeyListener() {
             @Override
@@ -41,44 +45,56 @@ public class MainActivity extends ActionBarActivity {
 
         });
 
-        final int sleep = 300;
         new Thread() {
             public void run() {
                 while (true) {
                     try {
-                        for (int i = 0; i < 40000; i += 100) {
-                            startTone(i);
-                            Thread.sleep(1000);
-                            Log.d("TAAG", "Frequency was " + i);
-                            stopTone();
-
-                        }
+                        startTone(400);
+                        Thread.sleep(10);
+                        startTone(800);
+                        Thread.sleep(10);
+                        startTone(1200);
+                        Thread.sleep(10);
+                        startTone(1600);
+                        Thread.sleep(10);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
             }
-        };
-//        }.start();
+//        };
+        }.start();
 
         AudioDispatcher dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(22050, 1024, 0);
 
         PitchDetectionHandler pdh = new PitchDetectionHandler() {
+            float lastPitch = -1;
             @Override
             public void handlePitch(PitchDetectionResult result, AudioEvent e) {
-                final float pitchInHz = result.getPitch();
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Log.d("TAG WHATEVER", pitchInHz + "");
+                float pitchInHz = result.getPitch();
+//                Log.d("TAG WHATEVER", ""+pitchInHz);
+//                Log.d("TAG WHATEVER", (char)(pitchInHz / 100) + "");
+
+                if (lastPitch > 0) {
+                    if (pitchInHz == -1) {
+                        Log.d("TAG WHATEVER", ""+lastPitch);
+                        Log.d("TAG WHATEVER", (char)(((lastPitch / 100) + 32)) + "");
+                        lastPitch = -1;
                     }
-                });
+                    lastPitch = pitchInHz;
+                }
+
+                if (pitchInHz > -1) {
+                    if (lastPitch == -1) {
+                        lastPitch = pitchInHz;
+                    }
+                }
             }
         };
-        AudioProcessor p = new PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.FFT_YIN, 22050, 1024, pdh);
-
-        dispatcher.addAudioProcessor(p);
-        new Thread(dispatcher, "Audio Dispatcher").start();
+//        AudioProcessor p = new PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.FFT_YIN, 22050, 1024, pdh);
+//
+//        dispatcher.addAudioProcessor(p);
+//        new Thread(dispatcher, "Audio Dispatcher").start();
 
 
     }
@@ -108,51 +124,44 @@ public class MainActivity extends ActionBarActivity {
 
     public void startTone(final double frequency) {
         isRunning = true;
-        t = new Thread() {
+        int buffsize = AudioTrack.getMinBufferSize(sr,
+                AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_16BIT);
+        // create an audiotrack object
+        AudioTrack audioTrack = new AudioTrack(
+                AudioManager.STREAM_MUSIC, sr,
+                AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_16BIT, buffsize,
+                AudioTrack.MODE_STREAM);
+
+        short samples[] = new short[buffsize];
+        int amp = 10000;
+        double twopi = 8. * Math.atan(1.);
+        double fr = frequency;
+        double ph = 0.0;
+        // start audio
+        audioTrack.play();
+
+        handler.postDelayed(new Runnable() {
+            @Override
             public void run() {
-                // set process priority
-//                setPriority(Thread.MAX_PRIORITY);
-                int buffsize = AudioTrack.getMinBufferSize(sr,
-                        AudioFormat.CHANNEL_OUT_MONO,
-                        AudioFormat.ENCODING_PCM_16BIT);
-                // create an audiotrack object
-                AudioTrack audioTrack = new AudioTrack(
-                        AudioManager.STREAM_MUSIC, sr,
-                        AudioFormat.CHANNEL_OUT_MONO,
-                        AudioFormat.ENCODING_PCM_16BIT, buffsize,
-                        AudioTrack.MODE_STREAM);
-
-                short samples[] = new short[buffsize];
-                int amp = 10000;
-                double twopi = 8. * Math.atan(1.);
-                double fr = frequency;
-                double ph = 0.0;
-                // start audio
-                audioTrack.play();
-
-                while (isRunning) {
-
-                    for (int i = 0; i < buffsize; i++) {
-                        samples[i] = (short) (amp * Math.sin(ph));
-                        ph += twopi * fr / sr;
-                    }
-                    audioTrack.write(samples, 0, buffsize);
-                }
-                audioTrack.stop();
-                audioTrack.release();
+                isRunning = false;
             }
-        };
-        t.start();
+        }, 10);
+        while (isRunning) {
+
+            for (int i = 0; i < buffsize; i++) {
+                samples[i] = (short) (amp * Math.sin(ph));
+                ph += twopi * fr / sr;
+            }
+            audioTrack.write(samples, 0, buffsize);
+        }
+        audioTrack.stop();
+        audioTrack.release();
     }
 
     public void stopTone() {
         isRunning = false;
-        try {
-            t.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        t = null;
     }
 
 
