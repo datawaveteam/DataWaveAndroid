6eb7f648b64dde78ad36fd72c6e479b9f65e4387
Stefan <soccerfanatic1996@gmail.com>
2015-01-17 22:41:10 -0500

    it worked

--- app/src/main/java/dwai/datawave/MainActivity.java
+++ app/src/main/java/dwai/datawave/MainActivity.java
@@ -10,155 +10,134 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
-import java.nio.charset.Charset;
-
-import be.tarsos.dsp.AudioDispatcher;
-import be.tarsos.dsp.AudioEvent;
-import be.tarsos.dsp.AudioProcessor;
-import be.tarsos.dsp.io.android.AudioDispatcherFactory;
-import be.tarsos.dsp.pitch.PitchDetectionHandler;
-import be.tarsos.dsp.pitch.PitchDetectionResult;
-import be.tarsos.dsp.pitch.PitchProcessor;
+import java.io.IOException;
+import java.nio.ByteBuffer;
 
 
 public class MainActivity extends ActionBarActivity {
+    public static final String ENCODER_DATA = "Hello World!! This text haas been encoded realtime and then fed to audio playback stream and the FSK decoder that actually displays it.! This text has been encoded realtime and then fed to audio playback stream and the FSK decoder that actually displays it.! This text has been encoded realtime and then fed to audio playback stream and the FSK decoder that actually displays it.! This text has been encoded realtime and then fed to audio playback stream and the FSK decoder that actually displays it.! This text has been encoded realtime and then fed to audio playback stream and the FSK decoder that actually displays it.! This text has been encoded realtime and then fed to audio playback stream and the FSK decoder that actually displays it. This text has been encoded realtime and then fed to audio playback stream and the FSK decoder that actually displays it.";
 
-    Handler handler;
+    protected FSKConfig mConfig;
+    protected FSKEncoder mEncoder;
+    protected FSKDecoder mDecoder;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        handler = new Handler();
-        final EditText urlEditText = ((EditText) findViewById(R.id.urlEditText));
-        urlEditText.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if (keyCode == KeyEvent.KEYCODE_ENTER) {
-                    editTextPressedEnter(urlEditText);
-                    return true;
-                }
-                return false;
-            }
+    protected AudioTrack mAudioTrack;
 
+    protected Runnable mDataFeeder = new Runnable() {
 
-        });
+        @Override
+        public void run() {
+            byte[] data = ENCODER_DATA.getBytes();
 
-        new Thread() {
-            public void run() {
-                while (true) {
+            if (data.length > FSKConfig.ENCODER_DATA_BUFFER_SIZE) {
+                //chunk data
+
+                byte[] buffer = new byte[FSKConfig.ENCODER_DATA_BUFFER_SIZE];
+
+                ByteBuffer dataFeed = ByteBuffer.wrap(data);
+
+                while (dataFeed.remaining() > 0) {
+
+                    if (dataFeed.remaining() < buffer.length) {
+                        buffer = new byte[dataFeed.remaining()];
+                    }
+
+                    dataFeed.get(buffer);
+
+                    mEncoder.appendData(buffer);
+
                     try {
-                        startTone(('Q'-32) * 100);
-
-                        Log.d("TAG STUFFFF AHH" , (('Q'-32) * 100) + "");
-                        Thread.sleep(10);
+                        Thread.sleep(100); //wait for encoder to do its job, to avoid buffer overflow and data rejection
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
             }
-//        };
-        }.start();
-
-        AudioDispatcher dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(22050, 1024, 0);
-
-        PitchDetectionHandler pdh = new PitchDetectionHandler() {
-            float lastPitch = -1;
-            @Override
-            public void handlePitch(PitchDetectionResult result, AudioEvent e) {
-                float pitchInHz = result.getPitch();
-//                Log.d("TAG WHATEVER", ""+pitchInHz);
-//                Log.d("TAG WHATEVER", (char)(pitchInHz / 100) + "");
-
-                if (lastPitch > 0) {
-                    if (pitchInHz == -1) {
-                        Log.d("TAG WHATEVER", ""+lastPitch);
-                        Log.d("TAG WHATEVER", (char)(((lastPitch / 100) + 32)) + "");
-                        lastPitch = -1;
-                    }
-                    lastPitch = pitchInHz;
-                }
-
-                if (pitchInHz > -1) {
-                    if (lastPitch == -1) {
-                        lastPitch = pitchInHz;
-                    }
-                }
-            }
-        };
-//        AudioProcessor p = new PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.FFT_YIN, 22050, 1024, pdh);
-//
-//        dispatcher.addAudioProcessor(p);
-//        new Thread(dispatcher, "Audio Dispatcher").start();
-
-
-    }
-
-
-    private void editTextPressedEnter(EditText editText) {
-        String url = editText.getText().toString();
-        byte[] urlBytes = url.getBytes(Charset.forName("US-ASCII"));
-        byte[][] urlBits = getBits(urlBytes);
-    }
-
-    private byte[][] getBits(byte[] bytes) {
-        final int AMOUNT_BITS_IN_ASCII = 7;
-        byte[][] bits = new byte[7][bytes.length];
-        for (int i = 0; i < bytes.length; i++) {
-            for (int j = 0; j < AMOUNT_BITS_IN_ASCII; j++) {
-                bits[i][j] = WaveUtil.getBit(j, bytes[i]);
+            else {
+                mEncoder.appendData(data);
             }
         }
-        return bits;
-    }
+    };
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    Thread t;
-    int sr = 44100;
-    boolean isRunning = true;
+        /// INIT FSK CONFIG
 
-    public void startTone(final double frequency) {
-        isRunning = true;
-        int buffsize = AudioTrack.getMinBufferSize(sr,
-                AudioFormat.CHANNEL_OUT_MONO,
-                AudioFormat.ENCODING_PCM_16BIT);
-        // create an audiotrack object
-        AudioTrack audioTrack = new AudioTrack(
-                AudioManager.STREAM_MUSIC, sr,
-                AudioFormat.CHANNEL_OUT_MONO,
-                AudioFormat.ENCODING_PCM_16BIT, buffsize,
+        try {
+            mConfig = new FSKConfig(FSKConfig.SAMPLE_RATE_44100, FSKConfig.PCM_16BIT, FSKConfig.CHANNELS_MONO, FSKConfig.SOFT_MODEM_MODE_4, FSKConfig.THRESHOLD_20P);
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        /// INIT FSK DECODER
+
+        mDecoder = new FSKDecoder(mConfig, new FSKDecoder.FSKDecoderCallback() {
+
+            @Override
+            public void decoded(byte[] newData ) {
+
+                final String text = new String(newData);
+
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        Log.d("The text", text);
+
+                    }
+                });
+            }
+        });
+
+        /// INIT FSK ENCODER
+
+        mEncoder = new FSKEncoder(mConfig, new FSKEncoder.FSKEncoderCallback() {
+
+            @Override
+            public void encoded(byte[] pcm8, short[] pcm16) {
+                if (mConfig.pcmFormat == FSKConfig.PCM_8BIT) {
+                    //8bit buffer is populated, 16bit buffer is null
+
+                    mAudioTrack.write(pcm8, 0, pcm8.length);
+
+                    mDecoder.appendSignal(pcm8);
+                }
+                else if (mConfig.pcmFormat == FSKConfig.PCM_16BIT) {
+                    //16bit buffer is populated, 8bit buffer is null
+
+                    mAudioTrack.write(pcm16, 0, pcm16.length);
+
+                    mDecoder.appendSignal(pcm16);
+                }
+            }
+        });
+
+        ///
+
+        mAudioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+                mConfig.sampleRate, AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_16BIT, 1024,
                 AudioTrack.MODE_STREAM);
 
-        short samples[] = new short[buffsize];
-        int amp = 10000;
-        double twopi = 8. * Math.atan(1.);
-        double fr = frequency;
-        double ph = 0.0;
-        // start audio
-        audioTrack.play();
+        mAudioTrack.play();
 
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                isRunning = false;
-            }
-        }, 10);
-        while (isRunning) {
+        ///
 
-            for (int i = 0; i < buffsize; i++) {
-                samples[i] = (short) (amp * Math.sin(ph));
-                ph += twopi * fr / sr;
-            }
-            audioTrack.write(samples, 0, buffsize);
-        }
-        audioTrack.stop();
-        audioTrack.release();
+        new Thread(mDataFeeder).start();
     }
 
-    public void stopTone() {
-        isRunning = false;
+    @Override
+    protected void onDestroy() {
+        mDecoder.stop();
+
+        mEncoder.stop();
+
+        mAudioTrack.stop();
+        mAudioTrack.release();
+
+        super.onDestroy();
     }
-
-
 }
